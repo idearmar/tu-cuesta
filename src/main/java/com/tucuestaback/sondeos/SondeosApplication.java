f6e7c753be45eaf1a71860809bf914b568eecb48
idearmar <62974969+idearmar@users.noreply.github.com>
2021-11-21 16:47:07 -0500

    Se añade modelo candidato

---
+++ src/main/java/com/tucuestaback/sondeos/SondeosApplication.java
@@ -0,0 +1,13 @@
+package com.tucuestaback.sondeos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SondeosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SondeosApplication.class, args);
+	}
+
+}
