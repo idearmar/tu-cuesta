9d20380e849b8797820efcfa79a446b069fecd21
idearmar <62974969+idearmar@users.noreply.github.com>
2021-11-27 09:26:57 -0500

    Se añade metodo de error CandidatoController

--- src/main/java/com/tucuestaback/sondeos/controllers/CandidatoController.java
+++ src/main/java/com/tucuestaback/sondeos/controllers/CandidatoController.java
@@ -6,11 +6,14 @@ import java.util.Map;
 
 import javax.validation.Valid;
 
+import com.tucuestaback.sondeos.exceptions.CustomException;
 import com.tucuestaback.sondeos.models.CandidatoModel;
 import com.tucuestaback.sondeos.services.CandidatoService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.Errors;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -35,5 +38,18 @@ public class CandidatoController {
     @GetMapping("/candidatos")
     public List<CandidatoModel> mostrar(){
         return candidatoService.obtenerCandidatos();
+    }
+
+    public void throwError(Errors error){
+        String mensaje="";
+        int index=0;
+
+        for(ObjectError e: error.getAllErrors()){
+            if(index>0){
+                mensaje +=" | ";
+            }
+            mensaje+=String.format("Parametro: %s - Mensaje: %s", e.getObjectName(),e.getDefaultMessage());
+        }
+        throw new CustomException(mensaje);
     }
 }
